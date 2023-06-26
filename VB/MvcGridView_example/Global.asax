023f3498b4bbf52be258003d586542d2c242d046
DevExpressExampleBot <fakeEmail>
2023-06-26 13:05:12 +0400

    Source auto update [skip ci]

--- VB/MvcGridView_example/Global.asax
+++ VB/MvcGridView_example/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="MvcGridView_example.MvcApplication" Language="vb" %>
+<%@ Application Codebehind="Global.asax.vb" Inherits="MvcGridView_example.MvcApplication" Language="VB" %>
--- VB/MvcGridView_example/Global.asax.vb
+++ VB/MvcGridView_example/Global.asax.vb
@@ -1,7 +1,4 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Text
 Imports System.Web
 Imports System.Web.Mvc
@@ -9,55 +6,52 @@ Imports System.Web.Routing
 Imports DevExpress.Web.ASPxClasses
 
 Namespace MvcGridView_example
-	' Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	' visit http://go.microsoft.com/?LinkId=9394801
 
-	Public Class MvcApplication
-		Inherits System.Web.HttpApplication
-		Public Shared Sub RegisterGlobalFilters(ByVal filters As GlobalFilterCollection)
-			filters.Add(New HandleErrorAttribute())
-		End Sub
+    ' Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    ' visit http://go.microsoft.com/?LinkId=9394801
+    Public Class MvcApplication
+        Inherits HttpApplication
 
-		Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+        Public Shared Sub RegisterGlobalFilters(ByVal filters As GlobalFilterCollection)
+            filters.Add(New HandleErrorAttribute())
+        End Sub
 
-			routes.MapRoute("Default", "{controller}/{action}/{id}", New With {Key .controller = "Home", Key .action = "Index", Key .id = UrlParameter.Optional})
+        Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+            routes.MapRoute("Default", "{controller}/{action}/{id}", New With {.controller = "Home", .action = "Index", .id = UrlParameter.Optional}) ' Route name
+        ' URL with parameters
+        ' Parameter defaults
+        End Sub
 
-		End Sub
+        Protected Sub Application_Start()
+            Call AreaRegistration.RegisterAllAreas()
+            RegisterGlobalFilters(GlobalFilters.Filters)
+            RegisterRoutes(RouteTable.Routes)
+            AddHandler ASPxWebControl.CallbackError, New EventHandler(AddressOf Application_Error)
+        End Sub
 
-		Protected Sub Application_Start()
-			AreaRegistration.RegisterAllAreas()
+        Private Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
+            Dim server As HttpServerUtility = HttpContext.Current.Server
+            Dim exception As Exception = server.GetLastError()
+            AddToLog(exception.Message, exception.StackTrace)
+        End Sub
 
-			RegisterGlobalFilters(GlobalFilters.Filters)
-			RegisterRoutes(RouteTable.Routes)
+        Private Sub AddToLog(ByVal message As String, ByVal stackTrace As String)
+            Dim sb As StringBuilder = New StringBuilder()
+            sb.AppendLine(Date.Now.ToLocalTime().ToString())
+            sb.AppendLine(message)
+            sb.AppendLine()
+            sb.AppendLine("Source File: " & HttpContext.Current.Request.RawUrl)
+            sb.AppendLine()
+            sb.AppendLine("Stack Trace: ")
+            sb.AppendLine(stackTrace)
+            For i As Integer = 0 To 150 - 1
+                sb.Append("-")
+            Next
 
-			AddHandler ASPxWebControl.CallbackError, AddressOf Application_Error
-		End Sub
-
-		Private Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
-			Dim server As HttpServerUtility = HttpContext.Current.Server
-			Dim exception As Exception = server.GetLastError()
-
-			AddToLog(exception.Message, exception.StackTrace)
-		End Sub
-
-		Private Sub AddToLog(ByVal message As String, ByVal stackTrace As String)
-			Dim sb As New StringBuilder()
-			sb.AppendLine(DateTime.Now.ToLocalTime().ToString())
-			sb.AppendLine(message)
-			sb.AppendLine()
-			sb.AppendLine("Source File: " & HttpContext.Current.Request.RawUrl)
-			sb.AppendLine()
-			sb.AppendLine("Stack Trace: ")
-			sb.AppendLine(stackTrace)
-			For i As Integer = 0 To 149
-				sb.Append("-")
-			Next i
-			sb.AppendLine()
-
-			Application("Log") += sb.ToString()
-
-			sb.AppendLine()
-		End Sub
-	End Class
+            sb.AppendLine()
+            Application("Log") += sb.ToString()
+            sb.AppendLine()
+        End Sub
+    End Class
 End Namespace
